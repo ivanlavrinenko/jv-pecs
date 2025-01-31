aa798ab1abadd5523172ee692d3432db0919acae
ivanlavrinenko <lavrinenkoivan51@gmail.com>
2025-01-31 07:55:06 +0100

    satisfaction

--- src/main/java/core/mate/academy/service/BulldozerProducer.java
+++ src/main/java/core/mate/academy/service/BulldozerProducer.java
@@ -4,10 +4,12 @@ import core.mate.academy.model.Bulldozer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BulldozerProducer implements MachineProducer {
+public class BulldozerProducer implements MachineProducer<Bulldozer> {
     @Override
     public List<Bulldozer> get() {
         List<Bulldozer> bulldozers = new ArrayList<>();
+        bulldozers.add(new Bulldozer());
+        bulldozers.add(new Bulldozer());
         bulldozers.add(new Bulldozer());
 
         return bulldozers;
